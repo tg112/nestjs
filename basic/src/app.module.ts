aa7485e44369de3457a5ba3658f62a58a72b4a7b
Shota Togawa <shotatogawa@Shotas-MacBook-Pro.local>
2026-01-05 17:23:14 -0800

    chore: remove unnecessary code

--- basic/src/app.module.ts
+++ basic/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
-import { ItemsController } from './items/items.controller';
-import { ItemsService } from './items/items.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [ItemsModule, PrismaModule, AuthModule],
-  controllers: [ItemsController],
-  providers: [ItemsService],
 })
 export class AppModule {}
